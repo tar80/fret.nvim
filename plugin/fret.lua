7b721ef3b375a5afdbc83b88c5e514b88b7efa5e
tar80 <tar80master@gmail.com>
2024-04-30 20:58:26 +0900

    feat!: change hlgroup name and setting method
    BREAKING CHANGE:
    These hlgroup names have changed
    - FretConfirm -> FretCandidateFirst
    - FretConfirmSub -> FretCandidateSecond
    - FretCandidate -> FretCandidateSub

--- plugin/fret.lua
+++ plugin/fret.lua
@@ -2,43 +2,45 @@ if vim.g.loaded_fret then
   return
 end
 
-local fret_augroup = vim.api.nvim_create_augroup('fret-nvim', { clear = true })
-local hl = {
-  { name = 'FretConfirm', fg = 'LightGreen', bg = 'NONE', deco = 'NONE' },
-  { name = 'FretConfirmSub', fg = 'DarkCyan', bg = 'NONE', deco = 'underline' },
-  { name = 'FretCandidate', fg = 'DarkCyan', bg = 'NONE', deco = 'NONE' },
-  { name = 'FretAlternative', fg = 'Black', bg = 'LightGreen', deco = 'NONE' },
-  { name = 'FretIgnore', fg = 'Gray', bg = 'NONE', deco = 'NONE' },
-}
-
 vim.g.loaded_fret = true
+
+local UNIQ_ID = 'fret-nvim'
+local hlgroup = {
+  ignore = 'FretIgnore',
+  first = 'FretCandidateFirst',
+  second = 'FretCandidateSecond',
+  sub = 'FretCandidateSub',
+  alt = 'FretAlternative',
+}
+local hl_detail = {
+  [hlgroup.ignore] = { fg = 'Gray', bg = 'NONE' },
+  [hlgroup.first] = { fg = 'LightGreen', bg = 'NONE', underline = true },
+  [hlgroup.second] = { fg = 'LightGreen', bg = 'DarkGreen' },
+  [hlgroup.sub] = { fg = 'DarkGreen', bg = 'NONE' },
+  [hlgroup.alt] = { fg = 'Black', bg = 'LightGreen' },
+}
+local augroup = vim.api.nvim_create_augroup(UNIQ_ID, { clear = true })
+
 vim.g.fret_enable_kana = false
 vim.g.fret_timeout = 0
 vim.g._fret_highlights =
-  { ['0'] = hl[5].name, ['1'] = hl[1].name, ['2'] = hl[2].name, ['3'] = hl[3].name, ['4'] = hl[4].name }
+  { ['0'] = hlgroup.ignore, ['1'] = hlgroup.first, ['2'] = hlgroup.second, ['3'] = hlgroup.sub, ['4'] = hlgroup.alt }
 
-local function set_hl()
-  local hl_detail
-  for _, v in ipairs(hl) do
-    hl_detail = string.format(
-      'default %s gui=%s guifg=%s guibg=%s cterm=%s ctermfg=%s ctermbg = %s',
-      v.name,
-      v.deco,
-      v.fg,
-      v.bg,
-      v.deco,
-      v.fg,
-      v.bg
-    )
-    vim.cmd.highlight({hl_detail, bang = true})
+---@param init? boolean
+local function set_hl(init)
+  for k, v in pairs(hl_detail) do
+    if init then
+      v.default = true
+    end
+    vim.api.nvim_set_hl(0, k, v)
   end
 end
 
 vim.api.nvim_create_autocmd({ 'ColorScheme' }, {
-  group = fret_augroup,
+  group = augroup,
   callback = function()
     set_hl()
   end,
 })
 
-set_hl()
+set_hl(true)
