418d92d09b10ac83f182f0a2355b1c5ace6dbcff
tar80 <tar80master@gmail.com>
2026-03-21 19:55:09 +0900

    ci: refine workflow implementation

--- tests/_min_init.lua
+++ tests/_min_init.lua
@@ -1,5 +1,5 @@
-vim.opt.encoding = "utf-8"
-vim.opt.fileencoding = "utf-8"
+vim.opt.encoding = 'utf-8'
+vim.opt.fileencoding = 'utf-8'
 vim.o.swapfile = false
 vim.o.undofile = false
 vim.o.backup = false
